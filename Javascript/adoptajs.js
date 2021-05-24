28ba1e1d9856968679b2264579d147f4aff76dcf
AlexandraBoghiu <72206052+AlexandraBoghiu@users.noreply.github.com>
2021-05-24 23:25:21 +0300

    Add files via upload

---
+++ Javascript/adoptajs.js
@@ -0,0 +1,7 @@
+let all = document.getElementsByClassName('numeAdoptie');
+for (let i = 0; i < all.length; i++) {
+  all[i].style.color = 	'black';
+  all[i].style.fontFamily = 'Times new roman';
+}
+
+
