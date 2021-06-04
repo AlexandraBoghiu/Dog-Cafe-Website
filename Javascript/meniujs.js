f0f21ccc8792709e4c8318652626da292231b498
AlexandraBoghiu <72206052+AlexandraBoghiu@users.noreply.github.com>
2021-06-04 21:41:59 +0300

    Add files via upload

---
+++ Javascript/meniujs.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click',(event) => {
+    console.log('Felicitări, trimite-ne un printscreen și te vom premia cu un voucher! Te așteptăm cu drag!');
+});
